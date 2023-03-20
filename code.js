cd82c3dfc3207eb87cb6f30d0af20769b401ecaf
ZahidK07 <zaksfit07@gmail.com>
2023-03-19 23:16:58 -0500

    first commit

---
+++ code.js
@@ -0,0 +1,19 @@
+
+// -------------- Nav Btn --------------
+function openMenu(){
+    document.getElementById("menuBtn").style.width ="100%"
+};
+
+function closeMenu(){
+    document.getElementById("menuBtn").style.width ="0%"
+}
+
+// -------------- Type Effect --------------
+    let typeEffect = new Typed(".multiText",{
+        strings: ["Developer.", "Coder."],
+        loop : true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 1500
+    });
+
