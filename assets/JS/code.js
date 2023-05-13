b09720c5e4994ffe53dba780a5b7c94095e3a823
ZahidK07 <zaksfit07@gmail.com>
2023-05-13 16:09:57 +0530

    Navbar logic added

--- assets/JS/code.js
+++ assets/JS/code.js
@@ -1,19 +1,35 @@
 
-// -------------- Nav Btn --------------
-function openMenu(){
-    document.getElementById("menuBtn").style.width ="100%"
-};
-
-function closeMenu(){
-    document.getElementById("menuBtn").style.width ="0%"
-}
-
 // -------------- Type Effect --------------
-    let typeEffect = new Typed(".multiText",{
-        strings: ["Developer.", "Coder."],
-        loop : true,
-        typeSpeed: 100,
-        backSpeed: 80,
-        backDelay: 1500
-    });
+let typeEffect = new Typed(".multiText",{
+    strings: ["Developer.", "Coder."],
+    loop : true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1500
+});
+
+// -------------- Transparent Mobile Menu --------------
+
+const menuBtn = document.querySelector('.MenuBtn');
+
+const navBar = document.querySelector('.navLinks');
+
+menuBtn.addEventListener('click',()=>{
+
+    let navVisible = navBar.getAttribute('data-visible');
+    console.log(navVisible)
+
+    if(navVisible === "false"){
+        navBar.setAttribute("data-visible", true);
+        menuBtn.setAttribute("aria-expanded", true);
+    } else{
+        navBar.setAttribute("data-visible", false);
+        menuBtn.setAttribute("aria-expanded", false);
+    }
+});
+
+
+
+
+
 
